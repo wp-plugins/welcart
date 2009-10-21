faeda18922f6f85d3762f70de60a82f99cc89e2f
uscnanbu <uscnanbu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-21 03:24:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/welcart/tags/0.1.0@165367 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ usc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Welcart
+Plugin URI: 
+Description: Welcart builds the management system with a net shop on Wordpress.
+Version: 0.1.1
+Author: USconsort
+Author URI: 
+*/
+?>
